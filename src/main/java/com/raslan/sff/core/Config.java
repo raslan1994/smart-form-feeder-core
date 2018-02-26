c6d1ccb4d60e360c9d1fb8d03c19267b5eb2b68a
raslanrauff <raslan1994@gmail.com>
2018-02-26 10:35:55 +0530

    Character Segmentation Impl

---
+++ src/main/java/com/raslan/sff/core/Config.java
@@ -0,0 +1,6 @@
+package com.raslan.sff.core;
+
+public class Config {
+	public static final int THRESHOLD = 100;
+	
+}
---
+++ src/main/java/com/raslan/sff/core/cs/Segmentation.java
@@ -0,0 +1,119 @@
+package com.raslan.sff.core.cs;
+
+import java.awt.image.BufferedImage;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.raslan.sff.core.Config;
+
+public class Segmentation {
+	public static boolean rowHasBlackPixel(BufferedImage img, int row){
+		for(int x = 0; x < img.getWidth(); x++){
+			int p = Math.abs(img.getRGB(x, row));
+			if(p > Config.THRESHOLD){return true;}
+		}
+		return false;
+	}
+	
+	public static boolean columnHasBlackPixel(BufferedImage img, int col){
+		for(int y = 0; y < img.getHeight(); y++){
+			int p = Math.abs(img.getRGB(col, y));
+			if(p > Config.THRESHOLD){return true;}
+		}
+		return false;
+	}
+	
+	public static List<BufferedImage> lineSegmentation(BufferedImage img){
+		int width = img.getWidth();
+		int y0 = -1;
+		int y1 = -1;
+		
+		List<BufferedImage> lines = new LinkedList<BufferedImage>();
+		
+		for(int y = 0; y <img.getHeight(); y++){
+			if(rowHasBlackPixel(img, y)){
+				if(y0 != -1 && y0 != y1){
+					y0 =y;
+				}
+				y1 = y;
+			}else if(y0 != -1 && y0 != y1){
+				BufferedImage subImg = img.getSubimage(0, y0, width, y1-y0);
+				lines.add(subImg);
+				
+				//reset
+				y0 = -1;
+				y1 = -1;
+			}
+		}
+		
+		//In case image ends on black pixel
+		if(y0 != -1 && y0 != y1){
+			BufferedImage subImg = img.getSubimage(0, y0, width, y1 - y0);
+			lines.add(subImg);
+		}
+		return lines;
+	}
+	
+	public static List<BufferedImage> characterSegmentation(BufferedImage img){
+		int height = img.getHeight()-1;
+		int x0 = -1;
+		int x1 = -1;
+		List<BufferedImage> chars = new LinkedList<BufferedImage>();
+		
+		for(int x = 0; x< img.getWidth(); x++){
+			if(columnHasBlackPixel(img, x)){
+				if(x0 == -1){x0=x;}
+				x1 = x;
+			}else if(x0 != -1 && x0 != x1){
+				BufferedImage subImg = img.getSubimage(x0, 0, x1-x0, height);
+				chars.add(subImg);
+				
+				//reset
+				x0 = -1;
+				x1 = -1;
+			}
+		}
+		
+		//In case image ends on black pixel
+		if(x0 != -1 && x0 != x1){
+			BufferedImage subImage = img.getSubimage(x0, 0, x1-x0, height);
+			chars.add(subImage);
+		}
+		
+		
+		return chars;
+	}
+	
+	public static BufferedImage trim(BufferedImage img){
+		int width = img.getWidth()-1;
+		int y0 = -1;
+		int y1 = -1;
+		
+		for(int y = 0; y < img.getHeight(); y++){
+			if(rowHasBlackPixel(img, y)){
+				if(y0 == -1){y0 =y;}
+				y1 = y;
+			}else if(y0 != -1 && y0 != y1){
+				return img.getSubimage(0, y0, width, y1 - y0);
+			}
+		}
+		
+		return img.getSubimage(0, y0, width, y1-y0);
+	}
+	
+	public List<BufferedImage> segment(BufferedImage img){
+		List<BufferedImage> seperatedChars = new LinkedList<BufferedImage>();
+		List<BufferedImage> lines = lineSegmentation(img);
+		
+		for(BufferedImage line : lines){
+			List<BufferedImage> chars = characterSegmentation(line);
+			
+			for(BufferedImage c : chars){
+				BufferedImage tc = trim(c);
+				seperatedChars.add(tc);
+			}
+		}
+		
+		return seperatedChars;
+	}
+}
