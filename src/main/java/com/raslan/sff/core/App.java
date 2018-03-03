d7fa76a931c4a8a9724f96c1ae99ae3868373e03
raslanrauff <raslan1994@gmail.com>
2018-03-03 15:43:35 +0530

    App configuration

--- src/main/java/com/raslan/sff/core/App.java
+++ src/main/java/com/raslan/sff/core/App.java
@@ -1,13 +1,46 @@
 package com.raslan.sff.core;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+import org.neuroph.core.data.DataSet;
+import org.neuroph.core.data.DataSetRow;
+
+import com.raslan.sff.core.data.DatasetLoader;
+import com.raslan.sff.core.data.LettersDataSetLoader;
+import com.raslan.sff.core.util.Logger;
+
 /**
- * Hello world!
+ * Primary execution class
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws URISyntaxException
     {
-        System.out.println( "Hello World!" );
+    	/*String trainPath = "/home/raslanrauff/ML/Dataset/english_bk/in/1";
+		DatasetLoader loader = new LettersDataSetLoader();
+		DataSet dataset = loader.loadDataSet(new File(trainPath).toPath());
+		
+		Iterator<DataSetRow> i = dataset.iterator();
+		
+		while(i.hasNext()){
+			DataSetRow ds = i.next();
+			System.out.println(ds.getLabel());
+			System.out.print("\n");
+			for(double d: ds.getInput()){
+				System.out.print(d);
+			}
+			System.out.print("\n");
+		}
+		
+		System.out.println(dataset.getInputSize());*/
+    	Logger logger = Logger.getInstance();
+    	logger.info("App", "Ends :)");
+    	
+		
     }
 }
