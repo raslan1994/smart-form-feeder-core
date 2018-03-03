e673afccdf4bb811da42e18553bde54b772af5d3
raslanrauff <raslan1994@gmail.com>
2018-03-03 14:57:33 +0530

    Character Mapping Support

---
+++ src/main/java/com/raslan/sff/core/data/CharacterMapper.java
@@ -0,0 +1,7 @@
+package com.raslan.sff.core.data;
+
+public interface CharacterMapper {
+	int mapCharacter(char character);
+    char mapInteger(int integer);
+    int getSize();
+}
---
+++ src/main/java/com/raslan/sff/core/data/NumberCharacterMapper.java
@@ -0,0 +1,5 @@
+package com.raslan.sff.core.data;
+
+public class NumberCharacterMapper {
+
+}
