806de110af4dcf98ff56228b750a418d13ceb8b1
raslanrauff <raslan1994@gmail.com>
2018-03-03 14:56:19 +0530

    Configuration

--- src/main/java/com/raslan/sff/core/Config.java
+++ src/main/java/com/raslan/sff/core/Config.java
@@ -2,5 +2,9 @@ package com.raslan.sff.core;
 
 public class Config {
 	public static final int THRESHOLD = 100;
-	
+	public static final String DATASET_TEST_PATH = "";
+	public static final String DATASET_TRAIN_PATH = "";
+	public static final String DEFAULT_NEURAL_NETWORK_NAME = "nn.nnet";
+	public static final double DEFAULT_LEARNING_RATE = 0.01d;
+	//public static final double CONFIDENT_LEVEL = 0.5;
 }
