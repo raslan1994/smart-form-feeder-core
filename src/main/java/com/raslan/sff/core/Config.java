476ef97536554e70616caa79288fcfc3eef2ea2d
raslanrauff <raslan1994@gmail.com>
2018-03-04 22:25:28 +0530

    Config update

--- src/main/java/com/raslan/sff/core/Config.java
+++ src/main/java/com/raslan/sff/core/Config.java
@@ -2,8 +2,8 @@ package com.raslan.sff.core;
 
 public class Config {
 	public static final int THRESHOLD = 100;
-	public static final String DATASET_TEST_PATH = "/home/raslanrauff/ML/Dataset/english/test/1";
-	public static final String DATASET_TRAIN_PATH = "/home/raslanrauff/ML/Dataset/english/train/13";
+	public static final String DATASET_TEST_PATH = "/home/raslanrauff/ML/Dataset/english/test/13";
+	public static final String DATASET_TRAIN_PATH = "/home/raslanrauff/ML/Dataset/english/train/1";
 	public static final String DEFAULT_NEURAL_NETWORK_NAME = "testNN.nnet";
 	public static final double DEFAULT_LEARNING_RATE = 0.01d;
 	public static final double DEFAULT_MOMENTUM = 0.025d;
