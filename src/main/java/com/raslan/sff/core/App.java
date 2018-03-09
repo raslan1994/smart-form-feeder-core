b7262b3f21c4a46b5d01f05a94e48b75052acf14
raslanrauff <raslan1994@gmail.com>
2018-03-09 23:14:18 +0530

    Maven run modification

--- src/main/java/com/raslan/sff/core/App.java
+++ src/main/java/com/raslan/sff/core/App.java
@@ -8,14 +8,23 @@ import java.net.URISyntaxException;
 import java.nio.file.Paths;
 import java.util.Iterator;
 
+import javax.imageio.ImageIO;
+
 import org.neuroph.core.data.DataSet;
 import org.neuroph.core.data.DataSetRow;
 
+import com.raslan.sff.core.data.BinaryImage;
+import com.raslan.sff.core.data.CharacterMapper;
 import com.raslan.sff.core.data.DatasetLoader;
+import com.raslan.sff.core.data.InputImage;
+import com.raslan.sff.core.data.LetterCharacterMapper;
 import com.raslan.sff.core.data.LettersDataSetLoader;
 import com.raslan.sff.core.nn.CharacterRecognizer;
 import com.raslan.sff.core.nn.Learner;
+import com.raslan.sff.core.nn.helper.DiagonalFeatureVector;
 import com.raslan.sff.core.util.Logger;
+import com.raslan.sff.ui.TemplatePreview;
+import com.raslan.sff.ui.data.FormTemplateManager;
 
 /**
  * Primary execution class
@@ -25,8 +34,36 @@ public class App
 {
     public static void main( String[] args ) throws URISyntaxException, IOException
     {
-    	IOHelper ioHelper = IOHelper.getInstance();
+    	/*CharacterMapper cm = new LetterCharacterMapper();
+    	BufferedImage img = ImageIO.read(new File("/home/raslanrauff/ML/Dataset/english/new_method/1/A1B.png"));
+    	BinaryImage bimg = new BinaryImage(img,null);
+    	InputImage iimg = new InputImage(bimg, 'A');*/
     	
+    	/*double[] v = bimg.getVector();
+    	System.out.println("Vector is");
+    	for(double v_ : v){
+    		System.out.print(""+Integer.valueOf(String.valueOf(v_).substring(0,1)));
+    	}
+    	System.out.print("\n");
+    	
+    	System.out.println("Matrix is ");
+    	int[][] ps = bimg.getMatrix();
+    	for(int i = 0; i < ps.length; i++){
+    		for(int x = 0; x < ps[i].length; x++){
+    			System.out.print(ps[i][x]+",");
+    		}
+    		System.out.print("\n");
+    	}
+    	System.out.print("\n");
+    	
+    	DiagonalFeatureVector dfv = new DiagonalFeatureVector(iimg, cm);
+    	double[] out = dfv.getInputs();
+    	System.out.println("Output");
+    	for(double d: out){
+    		System.out.println(d);
+    	}
+    	System.out.print("\n");*/
+    	//IOHelper ioHelper = IOHelper.getInstance();
     	/*DatasetLoader loader = new LettersDataSetLoader();
     	DataSet trainDs = loader.loadDataSet(new File(Config.DATASET_TRAIN_PATH).toPath());
     	DataSet testDs = loader.loadDataSet(new File(Config.DATASET_TEST_PATH).toPath());
@@ -41,7 +78,7 @@ public class App
 		
 		while(i.hasNext()){
 			DataSetRow ds = i.next();
-			System.out.println(ds.getLabel());
+			System.out.print(ds.getLabel());
 			System.out.print("\n");
 			for(double d: ds.getInput()){
 				System.out.print(d);
@@ -51,14 +88,22 @@ public class App
 		
 		System.out.println(dataset.getInputSize());*/
     	
-    	CharacterRecognizer cr = new CharacterRecognizer("/home/raslanrauff/ML/Dataset/NeuralNet_English.nnet");
+    	/*CharacterRecognizer cr = new CharacterRecognizer("/home/raslanrauff/ML/Dataset/NeuralNet_English.nnet");
 		BufferedImage img = ioHelper.getBufferedImageFromResource("ltr1.png");
 		
 		cr.recognizeLetter(img, 'l');
 		
 		
     	Logger logger = Logger.getInstance();
-    	logger.info("App", "Ends :)");
+    	logger.info("App", "Ends :)");*/
+    	
+    	/*FormTemplateManager manager = FormTemplateManager.getInstance();
+    	String path = "/media/raslanrauff/HP My Files/BSC_SE/Final/test_img/papers/img.jpg";
+    	manager.loadFromFile("/home/raslanrauff/smart-form-feeder/form_template_1.json");
+    	
+    	TemplatePreview preview = new TemplatePreview(manager);
+    	preview.changeImage(path);
+    	preview.setVisible(true);*/    	
     	
     }
 }
